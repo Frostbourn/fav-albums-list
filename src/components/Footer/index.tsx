cceb29becde47908d07b1b76c271441893dd639e
Jakub Skowroński <jakub.skowronski@rp.pl>
2023-02-06 22:57:39 +0100

    Change footer margins

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,8 +3,8 @@ import { StyledFooter } from "./styles";
 export const Footer = () => {
   return (
     <StyledFooter>
-      Made with <span className='text-danger'> ❤ </span> by{" "}
-      <a href='https://jakubskowronski.com' target='_blank'>
+      Made with <span className='text-danger mx-1'> ❤ </span> by{" "}
+      <a className="ms-1" href='https://jakubskowronski.com' target='_blank'>
         Jakub Skowronski
       </a>
     </StyledFooter>
